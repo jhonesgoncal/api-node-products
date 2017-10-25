c856f440463b6ca99e3a3bc189f8c5ef9da4a0db
Jhones Goncalves <jhones.goncalves@outlook.com>
2017-10-24 22:06:39 -0200

    model produtos e inserção no banco

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 const app = express();
 
+//Conecta ao Banco
+mongoose.connect('mongodb://jhonesgoncalves:41321158@ds040877.mlab.com:40877/nodestore');
+
+//Carrega os Models
+const Product = require('./models/product');
+
+//Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
